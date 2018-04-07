7bc5d2b6590adc9c79aef4e47a584668605ace6b
Jiawei Zhao <jonah@JiaweideMacBook-Pro.local>
2018-04-07 23:22:53 +0800

    Signum_tf 1.0

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Signum_tensorflow
+## This is the repository for Signum optimizer implemented in TensorFlow.
+### see the detailed discription of Signum in the original paper at: https://arxiv.org/abs/1711.05101
+
+Arguments:\
+        lr (float): learning rate\
+        momentum (float, optional): momentum factor (default: 0.9)\
+        weight_decay (float, optional): weight decay (default: 0)
+
+    Example:
+        >>> import tensorflow as tf
+        >>> your_loss = (eg: tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y_conv))
+        >>> optimizer = signum.Signum(lr=0.01, momentum=0.9, weight_decay = 0).minimize(your_loss)
+        >>> sess = tf.Session()
+        >>> loss, _ = sess.run([your_loss, optimizer])
+
+Note:\
+        The optimizer updates the weight by:\
+            momentum = beta * momentum + (1-beta)*rescaled_grad\
+            weight = (1 - lr * weight_decay) * weight - lr * sign(momentum)
+
+Considering the specific case of Momentum, the update Signum can be written as
+
+![](https://latex.codecogs.com/gif.latex?%5Cbegin%7Balign*%7D%20g_t%20%26%3D%20%5Cnabla%20J%28W_%7Bt-1%7D%29%5C%5C%20m_t%20%26%3D%20%5Cbeta%20m_%7Bt-1%7D%20&plus;%20%281%20-%20%5Cbeta%29%20g_t%5C%5C%20W_t%20%26%3D%20W_%7Bt-1%7D%20-%20%5Ceta_t%20%5Ctext%7Bsign%7D%28m_t%29%20%5Cend%7Balign*%7D)
+
+If do not consider Momentum, the update Sigsgd can be written as
+
+![](https://latex.codecogs.com/gif.latex?%5Cbegin%7Balign*%7D%20g_t%20%26%3D%20%5Cnabla%20J%28W_%7Bt-1%7D%29%5C%5C%20W_t%20%26%3D%20W_%7Bt-1%7D%20-%20%5Ceta_t%20%5Ctext%7Bsign%7D%28g_t%29%20%5Cend%7Balign*%7D)
+
+Description of example:\
+Tested on a standard TensorFlow example of MNIST dataset. (https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/neural_network.py)\
+train.py: train and validate the dataset\
+signum.py: contain the signum optimizer
---
+++ Signum.py
@@ -0,0 +1,99 @@
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import state_ops
+from tensorflow.python.training import optimizer
+import tensorflow as tf
+
+class Signum(optimizer.Optimizer):
+    r"""Implements Signum optimizer that takes the sign of gradient or momentum.
+
+        See details in the original paper at:https://arxiv.org/abs/1711.05101
+
+        Args:
+            params (iterable): iterable of parameters to optimize or dicts defining
+                parameter groups
+            lr (float): learning rate
+            momentum (float, optional): momentum factor (default: 0.9)
+            weight_decay (float, optional): weight decay (default: 0)
+
+        Example:
+            >>> import tensorflow as tf
+            >>> your_loss = (eg: tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y_conv))
+            >>> optimizer = signum.Signum(lr=0.01, momentum=0.9, weight_decay = 0).minimize(your_loss)
+            >>> sess = tf.Session()
+            >>> loss, _ = sess.run([your_loss, optimizer])
+
+        .. note::
+            The optimizer updates the weight by:
+                buf = momentum * buf + (1-momentum)*rescaled_grad
+                weight = (1 - lr * weight_decay) * weight - lr * sign(buf)
+
+            Considering the specific case of Momentum, the update Signum can be written as
+
+            .. math::
+                    \begin{split}g_t = \nabla J(W_{t-1})\\
+    			    m_t = \beta m_{t-1} + (1 - \beta) g_t\\
+    				W_t = W_{t-1} - \eta_t \text{sign}(m_t)}\end{split}
+
+            where p, g, v and :math:`\rho` denote the parameters, gradient,
+            velocity, and momentum respectively.
+
+            If do not consider Momentum, the update Sigsgd can be written as
+
+            .. math::
+                	g_t = \nabla J(W_{t-1})\\
+    				W_t = W_{t-1} - \eta_t \text{sign}(g_t)}
+
+        """
+
+    def __init__(self, lr=0.01, momentum=0.9, weight_decay=0, use_locking=False, name="Signum"):
+        super(Signum, self).__init__(use_locking, name)
+        self._lr = lr
+        self._momentum = momentum
+        self._weight_decay = weight_decay
+
+        if momentum == 0:
+            self.momentum_use = False
+        else:
+            self.momentum_use = True
+
+        if weight_decay == 0:
+            self.weight_decay_use = False
+        else:
+            self.weight_decay_use = True
+
+        # Tensor
+        self._lr_t = None
+        self._momentum_t = None
+        self._weight_decay_t = None
+
+    def _prepare(self):
+        self._lr_t = ops.convert_to_tensor(self._lr, name="learning_rate")
+        self._momentum_t = ops.convert_to_tensor(self._momentum, name="momentum")
+        self._weight_decay_t = ops.convert_to_tensor(self._weight_decay, name="weight_decay")
+
+    def _create_slots(self, var_list):
+        if self.momentum_use:
+            for v in var_list:
+                self._zeros_slot(v, "m", self._name)
+
+    def _apply_dense(self, grad, var):
+        lr_t = math_ops.cast(self._lr_t, var.dtype.base_dtype)
+        momentum_t = math_ops.cast(self._momentum_t, var.dtype.base_dtype)
+        weight_decay_t = math_ops.cast(self._weight_decay_t, var.dtype.base_dtype)
+
+        if self.momentum_use:
+            #signum
+            m = self.get_slot(var, "m")
+            m_t = m.assign(math_ops.mul(momentum_t, m) + math_ops.mul((1-momentum_t), grad))
+            if self.weight_decay_use:
+                decay = 1 - math_ops.mul(lr_t, weight_decay_t)
+                var = var.assign(var * decay)
+            var_update = state_ops.assign_sub(var, math_ops.mul(lr_t, tf.sign(m_t)))
+            return control_flow_ops.group(*[var_update, m_t])
+
+        else:
+            #signsgd
+            var_update = state_ops.assign_sub(var, math_ops.mul(lr_t, tf.sign(grad)))
+            return control_flow_ops.group(*[var_update])
